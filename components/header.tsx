dace87ffb300b2a5399df1e69bfb2f087f2eecd6
Jathurchan Selvakumar <admin@jathurchan.com>
2023-10-03 22:58:37 +0200

    Added components and .gitignore

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+
+export async function Header() {
+
+    return (
+        <header className="flex items-center justify-between w-full max-w-4xl px-8 py-4 mx-auto my-0">
+            <h2 className="text-2xl font-bold leading-tight text-gray-900">
+                Jathurchan Selvakumar
+            </h2>
+            <nav>
+            </nav>
+        </header>
+    )
+}
